821052086613c142eefeb11c48138e44fe04b92c
PJ <contact@pjm.design>
2021-10-20 12:44:33 -0500

    added comments and removed log

---
+++ README.md
@@ -0,0 +1,9 @@
+Plugin Link: https://www.figma.com/community/plugin/1032688500948147925
+
+How to use:
+
+1. Install the plugin via Figma or the Figma community page.
+
+2. Right click anywhere an the cavnas or on any layer in the layers panel.
+
+3. Click "Unlock All Layers" in the plugins category.
--- code.ts
+++ code.ts
@@ -1,20 +1,31 @@
 // This plugin unlocks all locked layers on the current page
 // I find this type of thing super useful in some cases, for instance: when replacing several locked background layers from an inported Sketch file. That's the exact situation I was in when I developed this plugin.
 
+
+// Get the current page
 const node = figma.currentPage;
 
+// Get all locked layers on the page
 const nodes = node.findAll(node => node.locked == true);
+
+// Number of locked layers on page
 const nodesno = nodes.length;
 
-console.log(nodesno);
+// console.log(nodesno);
 
-
+// If there are locked layers
 if (nodesno > 0) {
+
+	// Unlock them all!
 	nodes.forEach(i => {
 		i.locked = false;
 	});
+
+	// Tell user how many
 	alert(nodesno + ' layer'+(nodesno==1?' has':'s have')+' been unlocked!');
 } else {
+
+	// If there are no locked layers
 	alert('No layers are locked');
 }
 
